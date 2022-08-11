f313fc201cc63020050a0178571e878d1bdd59a3
rspaner <rpaner@ucsd.edu>
2022-08-11 13:58:07 -0700

    fixed spacing and created new nav bar

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function copyEmail() {
 	let text = "rachelp521@gmail.com";
 	navigator.clipboard.writeText(text);
 }
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
