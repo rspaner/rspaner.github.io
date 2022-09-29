d80fde1152a459336e07ddcd3775197b3d469f24
rspaner <rpaner@ucsd.edu>
2022-09-28 18:23:37 -0700

    added slideshow to homepage and gradients

--- index.js
+++ index.js
@@ -3,6 +3,37 @@ function copyEmail() {
   navigator.clipboard.writeText(text);
 }
 
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
+
+
+
 // 
 
 // var a = document.getElementById("test2")
@@ -19,8 +50,6 @@ var original = "../images/heart.png";
     "1": "../images/sculpting.png",
     "2": "../images/jewelry_making.png",
     "3": "../images/crochet.png"
-    //  "4": "https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    //  "5": "http://www.smallarmssurvey.org/uploads/tx_rgslideshow/sas-homepage-armed-violence.jpg"
   };
   var heart = document.getElementById("empty-heart");
   var arr = document.getElementsByClassName('fill');
@@ -30,8 +59,6 @@ var original = "../images/heart.png";
       var a = e.target;
       var imgId = a.getAttribute('data-image-id');
       var imgSrc = images[imgId];
-      // var style = ['background-image: url(', imgSrc, ');'].join('');
-      // background.setAttribute('style', style);
       heart.src = imgSrc;
       heart.setAttribute("style", "width: 350PX; height: 350px;");
 
