91532b87857bad3a22b114056ac26efec6566e8f
rspaner <rpaner@ucsd.edu>
2022-10-04 23:14:31 -0700

    added functionality to footer icons

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function copyEmail() {
-  let text = "rachelp521@gmail.com";
+  let text = "rachelspaner@gmail.com";
   navigator.clipboard.writeText(text);
 }
 
@@ -11,33 +11,3 @@ function copyEmail() {
 // var asdf = document.getElementById("test2");
 // console.log(asdf);
 
-var original = "../images/heart.png";
-
-
-(function () {
-  var images = {
-   
-    "1": "../images/sculpting.png",
-    "2": "../images/jewelry_making.png",
-    "3": "../images/crochet.png"
-  };
-  var heart = document.getElementById("empty-heart");
-  var arr = document.getElementsByClassName('fill');
-  // console.log(arr)
-  for (var i = 0; i < arr.length; i++) {
-    arr[i].onmouseover = function (e) {
-      var a = e.target;
-      var imgId = a.getAttribute('data-image-id');
-      var imgSrc = images[imgId];
-      heart.src = imgSrc;
-      heart.setAttribute("style", "width: 350PX; height: 350px;");
-
-    } 
-    arr[i].onmouseout = function (){
-    heart.src = "../images/heart.png";
-    }
-   
-  }
-
-})
-();
---
+++ pages/about.js
@@ -0,0 +1,30 @@
+var original = "../images/heart.png";
+
+
+(function () {
+  var images = {
+   
+    "1": "../images/sculpting.png",
+    "2": "../images/jewelry_making.png",
+    "3": "../images/crochet.png"
+  };
+  var heart = document.getElementById("empty-heart");
+  var arr = document.getElementsByClassName('fill');
+  // console.log(arr)
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].onmouseover = function (e) {
+      var a = e.target;
+      var imgId = a.getAttribute('data-image-id');
+      var imgSrc = images[imgId];
+      heart.src = imgSrc;
+      heart.setAttribute("style", "width: 350PX; height: 350px;");
+
+    } 
+    arr[i].onmouseout = function (){
+    heart.src = "../images/heart.png";
+    }
+   
+  }
+
+})
+();
